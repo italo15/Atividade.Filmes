22dd965a6e52c36743acef82d7eeb2610f705a25
zerowavez <lucastorinopam@gmail.com>
2026-02-27 14:46:11 -0300

    Implementação inicial do tratamento de erros na API

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,29 @@ const campoTitulo = document.querySelector("#titulo");
 const btnBusca = document.querySelector("#btnBusca")
 
 async function buscarFilme () {
-    const filme = campoTitulo.value !== null
-    ? campoTitulo.getAttribute(campoTitulo.value)
-    : '%def'; 
+    
+    try {
+        const filme = campoTitulo.value
 
-    const url = `https://www.omdbapi.com/?t=${filme}&apikey=ca15d384`
+        const resposta = await fetch(`https://www.omdbapi.com/?t=${filme}&apikey=ca15d384`);
+        if (!resposta.ok) {
+            throw new Error ("não foi possível concluir a busca")
+        }
+        
+        if (filme.length === 0) {
+            throw new Error ("insira um título")
+        }
 
-    const resposta = await fetch(url);
+        const dadosFilme = await resposta.json();
+        if (dadosFilme.Response === "False") {
+            throw new Error ("sem resultados")
+        }
 
-    const dadosFilme = await resposta.json();
-
-    console.log(dadosFilme);
+        console.log(dadosFilme);
+    }
+    catch(error) {
+        console.error(error);
+    }
 }
 
 const inputBusca = document.getElementById("titulo");
@@ -30,10 +42,10 @@ inputBusca.addEventListener("keydown", function(event) {
 // Renderizar os cards dinamicamente via manipulação do DOM
 // Exibir o modal com detalhes ao clicar em um card
 // Fechar o modal ao clicar no botão de fechar ou fora dele
-// Validar campo vazio: impedir busca e exibir mensagem de alerta
-// Tratar erro da API: mensagem amigável quando não há resultados (Response: 'False')
 
 //feitas:
+// ✅ Tratar erro da API: mensagem amigável quando não há resultados (Response: 'False')
+// ✅ Validar campo vazio: impedir busca e exibir mensagem de alerta
 // ✅ Ativar a busca ao pressionar Enter no campo de texto
 // ✅ Organizar o código em funções com responsabilidades claras (ex: buscarFilmes(), // renderCards(), abrirModal()
 // ✅ Usar fetch() com async/await para chamar a API de busca
